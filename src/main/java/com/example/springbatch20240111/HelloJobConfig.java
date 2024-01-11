5b3e3dd7bc3bee2502e3bb9c0f6eadb9172e7159
silvercho <tjddms01212@gmail.com>
2024-01-11 14:24:38 +0900

    미션1

---
+++ src/main/java/com/example/springbatch20240111/HelloJobConfig.java
@@ -0,0 +1,40 @@
+package com.example.springbatch20240111;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.batch.core.Job;
+
+    @Configuration
+    @Slf4j
+    public class HelloJobConfig {
+        @Bean
+        public Job simpleJob(JobRepository jobRepository, Step simpleStep) {
+            return new JobBuilder("simpleJob", jobRepository)
+                    .start(simpleStep)
+                    .build();
+        }
+
+        @Bean
+        public Step simpleStep(JobRepository jobRepository, Tasklet testTasklet,
+                               PlatformTransactionManager platformTransactionManager) {
+            return new StepBuilder("simpleStep", jobRepository)
+                    .tasklet(testTasklet, platformTransactionManager)
+                    .build();
+        }
+
+        @Bean
+        public Tasklet testTasklet() {
+            return ((contribution, chunkContext) -> {
+                log.info("Hello World");
+                return RepeatStatus.FINISHED;
+            });
+        }
+    }
