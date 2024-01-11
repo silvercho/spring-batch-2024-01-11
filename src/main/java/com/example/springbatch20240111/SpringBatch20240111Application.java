2aa7c2bb6842428d5ec2ad3d0e764fabd35d5d23
silvercho <tjddms01212@gmail.com>
2024-01-11 14:10:05 +0900

    start

---
+++ src/main/java/com/example/springbatch20240111/SpringBatch20240111Application.java
@@ -0,0 +1,13 @@
+package com.example.springbatch20240111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch20240111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch20240111Application.class, args);
+    }
+
+}
