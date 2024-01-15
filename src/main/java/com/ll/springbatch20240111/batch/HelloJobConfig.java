76265ea73996a2473c40ba566c6c4eb3f6330882
silvercho <tjddms01212@gmail.com>
2024-01-15 11:13:13 +0900

    hello

---
+++ src/main/java/com/ll/springbatch20240111/batch/HelloJobConfig.java
@@ -0,0 +1,42 @@
+package com.ll.springbatch20240111.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.batch.core.Job;
+
+@Slf4j
+@Configuration
+public class HelloJobConfig {
+    @Bean
+    public Job helloJob(JobRepository jobRepository, Step helloStep1) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep1)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloStep1Tasklet", jobRepository)
+                .tasklet(helloStep1Tasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Tasklet helloStep1Tasklet() {
+        return ((contribution, chunkContext) -> {
+            log.info("Hello World");
+            System.out.println("Hello World 1/1");
+            return RepeatStatus.FINISHED;
+        });
+    }
+}
